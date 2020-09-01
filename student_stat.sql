bd3e3e56afc5631f8a631d2a5aef509051ef6806
Sunny-NEU <32408063+Sunny-NEU@users.noreply.github.com>
2020-09-01 10:38:59 +0800

    Update student_stat.sql

--- student_stat.sql
+++ student_stat.sql
@@ -11,29 +11,29 @@ CREATE PROCEDURE calc_student_stat()
 CREATE TABLE student_stat as
 (
 select a.name,a.subject,a.teacher,a.score,b.total_score,concat(TRUNCATE((a.score AS DOUBLE)/b.total_score,2),'%') score_rate,c.avg_score
-from 
-(select t.id student_id,t1.subject_id,SUM(t1.score) total_score
-from student t
-left join score t1
-  on t.id=t1.student_id
-group by t.id,t1.subject_id) b
- LEFT JOIN
-  (select t1.student_id,t.name,t2.subject,t2.teacher,t1.score
-     from student t
-     left join score t1
-       on t.id=t1.student_id
-     left join subject t2
-       on t1.subject_id=t2.id
-    ORDER BY student_id) a
-       on a.student_id=b.student_id
-LEFT JOIN
-(select t.id,t1.subject_id,avg(t1.score) avg_score
-from student t
-left join score t1
-  on t.id=t1.student_id
-group by t.id,t1.subject_id
-) c
-on a.student_id=c.id
+  from 
+      (select t.id student_id,t1.subject_id,SUM(t1.score) total_score
+         from student t
+         left join score t1
+           on t.id=t1.student_id
+        group by t.id,t1.subject_id) b
+ left join
+         (select t1.student_id,t.name,t2.subject,t2.teacher,t1.score
+            from student t
+            left join score t1
+              on t.id=t1.student_id
+            left join subject t2
+              on t1.subject_id=t2.id
+           ORDER BY student_id) a
+   on a.student_id=b.student_id
+ left join
+      (select t.id,t1.subject_id,avg(t1.score) avg_score
+         from student t
+         left join score t1
+           on t.id=t1.student_id
+        group by t.id,t1.subject_id
+      ) c
+  on a.student_id=c.id
 )
 ;
 END$$
